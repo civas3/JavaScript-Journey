b089cb1de7df79c5ef3270e641166e2fc72abbbc
Paul C <57236805+civas3@users.noreply.github.com>
2021-04-21 17:30:56 +0100

    data.map()

---
+++ JS-data.map()-#1/js/app.js
@@ -0,0 +1,60 @@
+//all the data being stored in contactCardData array
+let contactCardData = [
+    {
+        name: 'Paul',
+        age: '28',
+        contact: '075123123123'
+    },
+    {
+        name: 'Eva',
+        age: '28',
+        contact: '075123123123'
+    },
+    {
+        name: 'Ted',
+        age: '29',
+        contact: '073323122123'
+    },
+    {
+        name: 'Pash',
+        age: '18',
+        contact: '075969122123'
+    },
+    {
+        name: 'John',
+        age: '47',
+        contact: '07599992123'
+    },
+    {
+        name: 'Dave',
+        age: '32',
+        contact: '07522222123'
+    },
+    {
+        name: 'Steve',
+        age: '61',
+        contact: '075777722123'
+    },
+    {
+        name: 'Jack',
+        age: '22',
+        contact: '075190909013'
+    },
+    {
+        name: 'Oleg',
+        age: '19',
+        contact: '075123122123'
+    }
+];
+
+//storing #contact-card div element inside cardBody variable
+const cardBody = document.querySelector('#contact-card');
+
+//map function, that goes trought contactCardData array
+let contactCardDetails = contactCardData.map(function(itemData){ //itemData can be any name
+    return '<div>' + 'Name: ' + itemData.name +  '<br> Age: '  + itemData.age + '<br> Number: ' + itemData.contact + '</div>';
+    
+});
+
+//displaying array data on the html page
+cardBody.innerHTML = contactCardDetails.join('\n');
