9a78d254595d9c29afdc2df68baf8b3964134987
Paul C <57236805+civas3@users.noreply.github.com>
2021-04-27 15:34:10 +0100

    for loop

--- JS-forLoop -#1/js/app.js
+++ JS-forLoop -#1/js/app.js
@@ -22,13 +22,13 @@ allCards.innerHTML = card.join('\n');
 
 
 
-// let cards = allCards.children;
-// for (i = 0; i < cards.length; i++) {
-//   if (cards[i].innerText.trim() === "color: red") {
-//     console.log("Found it")
-//     cards[i].classList.add("red");
-//   }
-// }
+let cards = allCards.children;
+for (i = 0; i < cards.length; i++) {
+  if (cards[i].innerText.trim() === "color: red") {
+    console.log("Found it")
+    cards[i].classList.add("red");
+  }
+}
 
 // .children Property gets a collection of the node (element)
 // .trim() method used to remove whitespace from both sides of a string:
